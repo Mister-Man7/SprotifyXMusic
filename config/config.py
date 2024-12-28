e1c1440e16f57a29fcc372dc3cce81c1e3e6ae1c
Antakara <86642530+Mister-Man7@users.noreply.github.com>
2024-12-28 09:41:55 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/Mister-Man7/SprotifyXMusic",
 )
 
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "v2")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
