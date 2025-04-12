d20f96328fe94652e9d7339c709887b3983ee0f8
Antakara <86642530+Mister-Man7@users.noreply.github.com>
2025-04-12 11:18:37 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -80,9 +80,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/Mister-Man7/SprotifyXMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "latest")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv(
@@ -93,10 +93,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/SprotifyNews"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/estropolis"
 )  # Example:- https://t.me/TheTeamVk
 
 
@@ -119,10 +119,10 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 # Time sleep duration For Telegram Downloader
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
-GLBAL_IMG_URL = "https://graph.org/file/afe0bf9e6b4fede3afc0e.jpg"
+GLBAL_IMG_URL = "https://graph.org/file/c9266ed41c1a9b8121185.jpg"
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheTeamVivek/YukkiMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/Estanislao/SprotifyMusic")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
